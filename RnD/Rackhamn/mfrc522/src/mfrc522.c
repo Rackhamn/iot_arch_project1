5e6c4af076114132095a2e0529ce44becfe05965
Rackhamn <carl.m.blumenthal@gmail.com>
2025-03-23 22:50:07 +0100

    Fixed write_reg and read_reg formatting

--- RnD/Rackhamn/mfrc522/src/mfrc522.c
+++ RnD/Rackhamn/mfrc522/src/mfrc522.c
@@ -12,7 +12,7 @@ uint8_t _sck, _mosi, _miso;
 uint8_t _cs, _rst;
 
 void write_reg(uint8_t reg, uint8_t val) {
-        uint8_t data[2] = { (reg << 1) & 0x7E, val };                                           
+        uint8_t data[2] = { (reg << 1) & 0x7E, val };
 	gpio_put(_cs, 0);
         spi_write_blocking(_spi, data, 2);
         gpio_put(_cs, 1);
@@ -22,8 +22,8 @@ uint8_t read_reg(uint8_t reg) {
         uint8_t addr = ((reg << 1) & 0x7E) | 0x80;
         uint8_t val = 0;
 
-        gpio_put(_cs, 0);
-        spi_write_blocking(_spi, &addr, 1);
+	gpio_put(_cs, 0);
+	spi_write_blocking(_spi, &addr, 1);
 	spi_read_blocking(_spi, 0x00, &val, 1);
         gpio_put(_cs, 1);
 
