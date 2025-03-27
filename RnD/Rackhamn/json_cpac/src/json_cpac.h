8d12e83111f6b48a8c5da39e9500a483c777d50e
Rackhamn <carl.m.blumenthal@gmail.com>
2025-03-27 19:20:05 +0100

    added token defines and token struct

--- RnD/Rackhamn/json_cpac/src/json_cpac.h
+++ RnD/Rackhamn/json_cpac/src/json_cpac.h
@@ -10,8 +10,31 @@
 // 	LE: 0x34, 0x12
 // 	BE: 0x12, 0x34
 
+#define JSON_TOKEN_STRING	0
+#define JSON_TOKEN_NUMBER	1
+#define JSON_TOKEN_TRUE		2
+#define JSON_TOKEN_FALSE	3
+#define JSON_TOKEN_NULL		4
+#define JSON_TOKEN_LBRACE	5  // {
+#define JSON_TOKEN_RBRACE	6  // }
+#define JSON_TOKEN_LBRACKET	7  // [
+#define JSON_TOKEN_RBRACKET	8  // ]
+#define JSON_TOKEN_COLON	9  // :
+#define JSON_TOKEN_COMMA	10 // ,
+#define JSON_TOKEN_EOF		11
+#define JSON_TOKEN_ERR		12
+typedef uint8_t token_type_t;
+
+struct json_token_s {
+	token_type_t type;
+	uint8_t * start; // if we use arena, maybe use [ base_ptr, offset ]
+	size_t len;
+};
+typedef struct json_token_s json_token_t;
+
+
 struct json_cpac_s {
-	
+	// arena_t * arena;
 };
 typedef struct json_cpac_s json_cpac_t;
 
