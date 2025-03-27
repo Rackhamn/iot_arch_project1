8527907734016f9e8b74a74cb85d675192798b6d
Rackhamn <carl.m.blumenthal@gmail.com>
2025-03-27 19:08:13 +0100

    added string8 comments + possible impl features

--- RnD/Rackhamn/json_cpac/src/string8.h
+++ RnD/Rackhamn/json_cpac/src/string8.h
@@ -12,6 +12,26 @@ struct string8_s {
 typedef struct string8_s string8_t;
 */
 
+/* TODO(@Rackhamn):
+ *	get num valid codepoints from ptr
+ *	get num invalid codepoints from ptr
+ *	search substring in ptr+size (from left and from right)
+ *	compare utf8 string
+ *	write utf8 string into buffer
+ *	trim utf8 buffer (remove ' ' or something)
+ *	concat utf8 buffers (with delimiter)
+ *	split utf8 buffer (with delimiter)
+ *	utf8 codepoint to upper / lower
+ *	copy utf8 string to buffer
+ *	get size of codepoint
+ *
+ *	+ auto remove emoji stuff?
+ *	+ use arena plz
+*/
+
+// maybe: 
+// 	get current codepoint and return ptr to next codepoint in buffer
+// uint8_t * utf8_codepoint(uint8_t * ptr, uint32_t * codepoint);
 // pointer to pointer
 uint32_t decode_utf8(uint8_t ** ptr) {
 	const uint8_t * p = *ptr;
@@ -19,23 +39,29 @@ uint32_t decode_utf8(uint8_t ** ptr) {
 
 	if(p[0] < 0x80) {
 		// ASCII
+		// 1 byte utf8 codepoint
 		codepoint = p[0];
 		*ptr += 1;
 	} else if((p[0] & 0xE0) == 0xC0) {
+		// 2 byte utf8 codepoint
 		codepoint = ((p[0] & 0x1F) << 6) | (p[1] & 0x3F);
 		*ptr += 2;
 	} else if((p[0] & 0xF0) == 0xE0) {
+		// 3 byte utf8 codepoint
 		codepoint = ((p[0] & 0x0F) << 12) |
 				((p[1] & 0x3F) << 6) |
 				(p[2] & 0x3F);
 		*ptr += 3;
 	} else if((p[0] & 0xF8) == 0xF0) {
+		// 4 byte utf8 codepoint
 		codepoint = ((p[0] & 0x07) << 18) |
 				((p[1] & 0x3F) << 12) |
 				((p[2] & 0x3F) << 6) |
 				(p[3] & 0x3F);
 		*ptr += 4;
 	}
+
+	// maybe just return the current ptr?
 	return codepoint;
 }
 
