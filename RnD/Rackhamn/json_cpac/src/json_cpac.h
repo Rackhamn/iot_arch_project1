bcb31b5c4b20445dc11d98ae02fe1169e9443f42
Rackhamn <carl.m.blumenthal@gmail.com>
2025-03-27 19:11:15 +0100

    added comments in json_cpac

--- RnD/Rackhamn/json_cpac/src/json_cpac.h
+++ RnD/Rackhamn/json_cpac/src/json_cpac.h
@@ -2,6 +2,13 @@
 #define JSON_CPAC_H
 
 #include "string8.h"
+// TODO(@Rackhamn):
+// 	use arena 
+// 	use string8 (utf8 lib)
+// + parse "U+1234" string into special split codepoint
+// 	and handle endianess on it for uint16_t conversion!
+// 	LE: 0x34, 0x12
+// 	BE: 0x12, 0x34
 
 struct json_cpac_s {
 	
