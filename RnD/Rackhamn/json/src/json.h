6bdffbb91e849188d52e916d4e4028e360df5ab5
Rackhamn <carl.m.blumenthal@gmail.com>
2025-04-03 16:27:01 +0200

    added higher level json header and updates test build script

---
+++ RnD/Rackhamn/json/src/json.h
@@ -0,0 +1,13 @@
+#ifndef JSON_H
+#define JSON_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../../arena/arena.h"
+#include "json_cpac.h"
+#include "json_make.h"
+#include "json_dump.h"
+#include "json_write.h"
+
+#endif /* JSON_H */
