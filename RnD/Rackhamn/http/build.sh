dcad9f902c38888a4dc46e4793f00f2abce2ee00
Rackhamn <carl.m.blumenthal@gmail.com>
2025-04-06 22:24:22 +0200

    added build script - with future fluff

---
+++ RnD/Rackhamn/http/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "build.sh!"
+
+ARENA_DIR="../../arena/"
+ARENA_C="../../arena/arena.c"
+
+SRC_DIR="src/"
+OUT_DIR="build/"
+
+# rmdir $OUT_DIR
+mkdir $OUT_DIR
+
+SRC_FILES=""
+INC_FILES=""
+
+# hashmap - 4x num of elements (pref pow2), not bucketed, large buffer only
+
+gcc src/http.c -o http -DPORT=8082 -lpthread -lc
