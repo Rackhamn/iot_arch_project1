0a1c73e90bf90dbe679ebcd737e5866bdd03ac73
Rackhamn <carl.m.blumenthal@gmail.com>
2025-04-08 01:41:45 +0200

    added HTTP response status codes in comment

--- RnD/Rackhamn/http/src/http.c
+++ RnD/Rackhamn/http/src/http.c
@@ -534,6 +534,75 @@ ROOT_DIR + "/img/*.png"
 ROOT_DIR + "/img/*.svg"
 */
 
+/*
+	* https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
+ 	100 Continue
+	101 Switching Protocols
+	102 Processing
+	103 Early Hints
+
+	200 OK
+	201 Created
+	202 Accepted
+	203 Non-Authoritative Information
+	204 No content
+	205 Reset Content
+	206 Partial Content
+	207 Multi-Status
+	208 Already Reported
+	226 IM Used
+
+	300 Multiple Choices
+	301 Moved Permanently
+	302 Found
+	303 See Other
+	304 Not Modified
+	305 Use Proxy *
+	306 Unused
+	307 Temporary Redirect
+	308 Permanent Redirect
+	
+	400 Bad Request
+	401 Unauthorized
+	402 Payment Required
+	403 Forbidden
+	404 Not Found
+	405 Method Not Allowed
+	406 Not Acceptable
+	407 Proxy Authentication Required
+	408 Request Timeout
+	409 Conflict
+	410 Gone
+	411 Length Required
+	412 Precondition Failed
+	413 Content Too Large
+	414 URI Too Large
+	416 Range Not Satisfiable
+	417 Expectation Failed
+	418 I'm a teapot
+	421 Misdirected Request
+	422 Unrpocessable Content
+	423 Locked
+	424 Failed Dependency
+	425 Too Eearly
+	428 Precondition Required
+	429 Too Many Requests
+	431 Request Header Fields Too Large
+	451 Unavailable For Legal Reasons
+
+	500 Internal Server Error
+	501 Not Implemented
+	502 Bad Gateway
+	503 Service Unavailable
+	504 Gateway Timeout
+	505 HTTP Version Not Supported
+	506 Variant Also Negotiates
+	506 Insufficient Storage
+	508 Loop Detected
+	510 Not Exetended
+	511 Network Authentication Required
+*/
+
 // TODO: move into code indexed mapped table
 #define STRLEN(s) (sizeof(s) - 1)
 
